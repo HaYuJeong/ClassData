bee5f7d19886235b6b5a0b2994bb5ebe851b1a3d
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-04-08 20:42:23 +0900

    Monolothic
    Monolothic

---
+++ 08_SI/03_Monolothic_Shop/fronted_vue/src/services/shop/simple-product/SimpleProductService.js
@@ -0,0 +1,10 @@
+// SimpleProductService.js
+// axios CRUD 공통 함수
+// 상품 공통 함수
+import http from "@/utils/http-common";
+
+class SimpleProductService{
+    // TODO: 전체 조회
+}
+
+export default new SimpleProductService; // 다른 파일에서 쓸 수 있도록 export default
